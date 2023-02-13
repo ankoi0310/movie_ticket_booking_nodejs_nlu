91891eb4472259255e84329d5ef75e9a557f306d
unknown <huynhvahuuan3620@gmail.com>
2023-02-14 01:58:43 +0700

    change package info

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ import express from 'express';
 import path from 'path';
 import { config } from 'dotenv';
 
-config({ path: path.resolve(__dirname, '../.env') });
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', require('./routes/app.routes'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.SERVER_PORT, () => {
- console.log('Server is running on port ' + process.env.SERVER_PORT);
+app.listen(SERVER_PORT, () => {
+ console.log(`Server was running on port ${SERVER_PORT}`);
 });
